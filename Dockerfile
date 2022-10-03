708e994571af70becf34fb4bf1f2b34703e570ff
Adam Sranko <srankoadam@gmail.com>
2022-10-03 21:25:08 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:latest
+
+ENV resource_limiter_port=50
+
+RUN mkdir -p /app
+ADD . /app
+WORKDIR /app
+
+EXPOSE 5000
+
+RUN pip install --no-cache-dir --upgrade pip \
+  && pip install --no-cache-dir -r requirements.txt
+
+RUN export FLASK_APP=main.py
+CMD ["python", "./main.py"]
---
+++ main.py
@@ -0,0 +1,68 @@
+from glob import glob
+import json
+import math
+from flask import Flask,request
+
+cap_for_buildings = 0.3
+cap_for_others = 0.9 - cap_for_buildings
+fleetValue = 0
+allowanceResources = {'Metal': 0, 'Crystal': 0, 'Deuterium': 0}
+allowanceShips = {'Metal': 0, 'Crystal': 0, 'Deuterium': 0}
+
+port = 5000
+app = Flask(__name__)
+
+def countDeuteriumHardCap():
+    global fleetValue
+    return fleetValue * 0.03
+
+def getAllowanceWithNoShipyard(availableResources):
+    global allowanceResources
+    global allowanceShips
+    
+    allowanceResources['Metal'] = math.ceil(availableResources['Metal'])
+    allowanceResources['Crystal'] = math.ceil(availableResources['Crystal'])
+    allowanceResources['Deuterium'] = math.ceil(availableResources['Deuterium'])
+    allowanceShips = {'Metal': 0, 'Crystal': 0, 'Deuterium': 0}
+
+def getDefaultAllowance(resources):
+    global allowanceResources
+    
+    allowanceResources['Metal'] = math.ceil(resources['Metal'] * cap_for_buildings)
+    allowanceResources['Crystal'] = math.ceil(resources['Crystal'] * cap_for_buildings)
+    allowanceResources['Deuterium'] = math.ceil(resources['Deuterium'] * cap_for_buildings)
+
+def getDefaultShipAllowance(resources):
+    global allowanceShips
+    
+    allowanceShips['Metal'] = math.ceil(resources['Metal'] * cap_for_others)
+    allowanceShips['Crystal'] = math.ceil(resources['Crystal'] * cap_for_others)
+    allowanceShips['Deuterium'] = math.ceil(resources['Deuterium'] * cap_for_others - countDeuteriumHardCap())
+
+@app.route('/get_allowances', methods=['GET'])
+def getAllowances():
+    global fleetValue
+    global allowanceResources
+    global allowanceShips
+
+    request_data = request.get_json()
+
+    resources = request_data['resources']
+    facilities = request_data['facilities']
+    fleetValue = request_data['fleetValue']
+
+    getDefaultShipAllowance(resources)
+
+    if(facilities['Shipyard'] < 2):
+        getAllowanceWithNoShipyard(resources)
+    else:
+        getDefaultAllowance(resources)
+
+    allowanceResourcesWithHeader =  {'allowanceResources': allowanceResources}
+    allowanceShipsWithHeaders =  {'allowanceShips': allowanceShips}
+
+    returnAllowances = {**allowanceResourcesWithHeader, **allowanceShipsWithHeaders}
+    print(returnAllowances)
+    return returnAllowances
+
+app.run(port=port)
