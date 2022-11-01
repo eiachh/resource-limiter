5312e35c691022f9607e589fd642d8915244a957
Adam Sranko <srankoadam@gmail.com>
2022-11-01 17:29:42 +0100

    Added git check for build script

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+userInput="n"
+git fetch
+gitstatus=$(git status)
+if [[ $gitstatus == *"Your branch is behind"* ]]; then
+    echo "Your branch is behind, do you want to pull first? (y/n)"
+    read userInput
+fi
+
+if [[ $userInput == "y" ]]; then
+    git pull
+fi
+
+git submodule update --init --recursive
+
 eval $(minikube docker-env)
 docker build -t eiachh/resource-limiter ./
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
